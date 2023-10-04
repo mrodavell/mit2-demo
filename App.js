8353ec1b92155ed92d3ecc92019d8dbb053850f0
mrodavell <jomar.llevado@ustp.edu.ph>
2023-10-04 14:55:58 +0800

    New React Project

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.colorRed}>Jomar C. Llevado</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> 
   );
 }
 
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  colorRed:{
+    color: 'red',
+    fontSize: 80
+  }
 });
