8a39602f2221a4103c4c7d5f4147f9cf18812165
mrodavell <jomar.llevado@ustp.edu.ph>
2023-11-04 11:28:29 +0800

    added tailwind

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { View, Text, StyleSheet } from "react-native";
-import Header from "./src/components/Header";
-import Content from "./src/components/Content";
-
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
-    <View style={styles.box}>
-      <Header />
-      <Content /> 
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+      <StatusBar style="dark" />
+      <View className="flex-1 justify-center">
+        <View>
+          <Text className="text-red-600">Item 1</Text>
+        </View>
+        <View>
+          <Text>Item 2</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  box: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    flexGrow: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: "row",
+//     flex: 1,
+//     justifyContent: "flex-start",
+//     alignItems: "flex-start",
+//   },
+//   item: {
+//     height: 100,
+//     width: 100,
+//     borderColor: "#000",
+//     borderWidth: 2,
+//   },
+// });
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,39 +1,29 @@
-import { View, TextInput, Button } from "react-native";
-import SwitchButton from "./ui/SwitchButton";
-
+import { View, TextInput, Button, StyleSheet } from "react-native";
 export default function Content() {
   return (
     <View>
-      <TextInput
-        placeholder="Email"
-        style={{
-          borderWidth: 1,
-          width: 250,
-          marginTop: 15,
-          height: 50,
-          paddingLeft: 10,
-          marginBottom: 10,
-        }}
-      />
-      <TextInput
-        placeholder="Password"
-        style={{
-          borderWidth: 1,
-          width: 250,
-          height: 50,
-          paddingLeft: 10,
-          marginBottom: 10,
-        }}
-      />
+      <View style={{ marginTop: 15 }}>
+        <TextInput placeholder="Email" style={styles.textInput} />
+      </View>
+      <View>
+        <TextInput placeholder="Password" style={styles.textInput} />
+      </View>
       <View>
         <Button title="Login" />
       </View>
       <View style={{ marginTop: 5 }}>
         <Button title="Sigup" />
       </View>
-      <View>
-        <SwitchButton />
-      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    width: 250,
+    height: 50,
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+});
