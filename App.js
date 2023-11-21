cff82eff2b6131c2772c86b01213fca6672f5cbf
mrodavell <jomar.llevado@ustp.edu.ph>
2023-11-21 08:23:19 +0800

    state management

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Component1 from "./src/components/Component1";
-
+import React from "react";
 export default function App() {
+  const [init, setInit] = React.useState(0);
+
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
       <Text>Test State Management</Text>
-      <Component1 />
+      <Component1 init={init} />
     </SafeAreaView>
   );
 }
